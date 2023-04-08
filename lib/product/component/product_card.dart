04845194827e5933f6b7a2b73ed6742c081e760a
lsr <sirorong2@gmail.com>
2023-04-08 15:59:07 +0900

    RestaurantDetailModel에 JsonSerializable 적용해보기

--- lib/product/component/product_card.dart
+++ lib/product/component/product_card.dart
@@ -1,8 +1,32 @@
 import 'package:actual/common/const/colors.dart';
+import 'package:actual/restaurant/model/restaurant_detail_model.dart';
 import 'package:flutter/material.dart';
 
 class ProductCard extends StatelessWidget {
-  const ProductCard({Key? key}) : super(key: key);
+  final Image image;
+  final String name;
+  final String detail;
+  final int price;
+
+  const ProductCard(
+      {Key? key, required this.image, required this.name, required this.detail, required this.price})
+      : super(key: key);
+
+  factory ProductCard.fromModel({
+    required RestaurantProductModel model,
+  }){
+    return ProductCard(
+      image: Image.network(
+        model.imgUrl,
+        width: 110,
+        height: 110,
+        fit: BoxFit.cover,
+      ),
+      name: model.name,
+      detail: model.detail,
+      price: model.price,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,13 +34,8 @@ class ProductCard extends StatelessWidget {
       child: Row(
         children: [
           ClipRRect(
-            borderRadius: BorderRadius.circular(8.0),
-            child: Image.asset(
-              'asset/img/food/ddeok_bok_gi.jpg',
-              width: 110,
-              height: 110,
-              fit: BoxFit.cover,
-            ),
+              borderRadius: BorderRadius.circular(8.0),
+              child: image,
           ),
           const SizedBox(
             width: 16.0,
@@ -27,14 +46,14 @@ class ProductCard extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                  '떡볶이',
+                  name,
                   style: TextStyle(
                     fontSize: 18.0,
                     fontWeight: FontWeight.w500,
                   ),
                 ),
                 Text(
-                  '전통 떡볶이의 정석!\n맛잇습니다.맛잇습니다.맛잇습니다.맛잇습니다.맛잇습니다.맛잇습니다.맛잇습니다.맛잇습니다.맛잇습니다.',
+                  detail,
                   overflow: TextOverflow.ellipsis,
                   maxLines: 2,
                   style: TextStyle(
@@ -43,7 +62,7 @@ class ProductCard extends StatelessWidget {
                   ),
                 ),
                 Text(
-                  '\10000',
+                  '₩ $price',
                   textAlign: TextAlign.right,
                   style: TextStyle(
                     color: PRIMARY_COLOR,
--- lib/restaurant/view/restaurant_detail_screen.dart
+++ lib/restaurant/view/restaurant_detail_screen.dart
@@ -38,11 +38,13 @@ class RestaurantDetailScreen extends StatelessWidget {
             print(snapshot.data);
 
             if (!snapshot.hasData) {
-              return Center(child: CircularProgressIndicator(),);
+              return Center(
+                child: CircularProgressIndicator(),
+              );
             }
 
             final item = RestaurantDetailModel.fromJson(
-              json: snapshot.data!,
+              snapshot.data!,
             );
 
             return CustomScrollView(
@@ -51,7 +53,9 @@ class RestaurantDetailScreen extends StatelessWidget {
                   model: item,
                 ),
                 renderLabel(),
-                renderProducts(),
+                renderProducts(
+                  products: item.products,
+                ),
               ],
             );
           },
@@ -73,18 +77,23 @@ class RestaurantDetailScreen extends StatelessWidget {
     );
   }
 
-  SliverPadding renderProducts() {
+  SliverPadding renderProducts({
+    required List<RestaurantProductModel> products,
+  }) {
     return SliverPadding(
       padding: EdgeInsets.symmetric(horizontal: 16.0),
       sliver: SliverList(
         delegate: SliverChildBuilderDelegate(
           (context, index) {
+            final model = products[index];
             return Padding(
               padding: const EdgeInsets.only(top: 16.0),
-              child: ProductCard(),
+              child: ProductCard.fromModel(
+                model: model,
+              ),
             );
           },
-          childCount: 10,
+          childCount: products.length,
         ),
       ),
     );
@@ -92,7 +101,7 @@ class RestaurantDetailScreen extends StatelessWidget {
 
   SliverToBoxAdapter renderTop({
     required RestaurantDetailModel model,
-}) {
+  }) {
     return SliverToBoxAdapter(
       child: RestaurantCard.fromModel(
         model: model,
